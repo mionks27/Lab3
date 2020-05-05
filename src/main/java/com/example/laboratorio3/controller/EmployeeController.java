2fec819c3ea7466291a3a17b526e2ba2da41b7d2
Anthony Rojas <a20145976@pucp.pe>
2020-05-04 20:55:28 -0500

    final

--- src/main/java/com/example/laboratorio3/controller/EmployeeController.java
+++ src/main/java/com/example/laboratorio3/controller/EmployeeController.java
@@ -6,8 +6,10 @@ import com.example.laboratorio3.repository.JobRepository;
 import com.example.laboratorio3.repository.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/employee")
 public class EmployeeController {
 
     @Autowired
